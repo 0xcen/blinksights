6ecfb8dbcd34c40f0be1c4d079670f9fcd345564
Carlos <75177443+0xcen@users.noreply.github.com>
2024-08-11 15:45:22 +0200

    wip

--- src/app/blinks/[id]/page.tsx
+++ src/app/blinks/[id]/page.tsx
@@ -1,32 +1,14 @@
 "use client";
 import { useParams } from "next/navigation";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import useBlink from "../../../hooks/useBlink";
-import NumberCard from "../../../components/NumberCard";
-=======
 import NumberCard from "../../../components/NumberCard";
 import useBlink from "../../../hooks/useBlink";
 import BreadcrumbNav from "../../../components/BreadcrumbNav";
->>>>>>> Stashed changes
-=======
-import NumberCard from "../../../components/NumberCard";
-import useBlink from "../../../hooks/useBlink";
-import BreadcrumbNav from "../../../components/BreadcrumbNav";
->>>>>>> Stashed changes
 
 const Page = () => {
   const { id } = useParams();
   const blink = useBlink(id as string);
 
   return (
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    <div>
-      <h1>{blink.data?.id}</h1>
-=======
-=======
->>>>>>> Stashed changes
     <div className="space-y-2">
       <BreadcrumbNav
         items={[
@@ -38,10 +20,6 @@ const Page = () => {
       />
 
       <h1>{blink.data?.title} Blink</h1>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
       <div className="flex gap-2">
         <NumberCard
           title={"Views"}
